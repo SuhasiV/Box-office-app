25a8be2ca9afffcb14836d5d45649283e94e348f
Suhasi Verma <suhasiverma00@gmail.com>
2023-06-07 23:39:11 +0530

    fetched starred shows data and diaplay showgrid for them

--- src/api/tvmaze.js
+++ src/api/tvmaze.js
@@ -15,3 +15,8 @@ export const searchForPeople = query => apiGet(`/search/people?q=${query}`);
 
 export const getShowById = showId =>
   apiGet(`/shows/${showId}?embed[]=seasons&embed[]=cast`);
+
+export const getShowsByIds = async showIds => {
+  const promises = showIds.map(showId => apiGet(`/shows/${showId}`));
+  return Promise.all(promises);
+};
